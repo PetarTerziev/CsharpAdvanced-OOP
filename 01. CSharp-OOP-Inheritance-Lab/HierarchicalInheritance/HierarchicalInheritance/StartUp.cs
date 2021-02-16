9adff6534bec750ef5491925fb5e93e92d704fa4
PetarTerziev <74399708+PetarTerziev@users.noreply.github.com>
2021-02-16 12:24:53 +0200

    Add new project
    Lesson 01 Lab

---
+++ 01. CSharp-OOP-Inheritance-Lab/HierarchicalInheritance/HierarchicalInheritance/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+
+            cat.Eat();
+            cat.Meow();
+
+            dog.Eat();
+            dog.Bark();
+            
+        }
+    }
+}
---
+++ 01. CSharp-OOP-Inheritance-Lab/RandomList/RandomList/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("25");
+            list.Add("-55");
+            list.Add("1001");
+            list.Add("-555555");
+
+
+            Console.WriteLine(list.Count);
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.Count);
+
+        }
+    }
+}
